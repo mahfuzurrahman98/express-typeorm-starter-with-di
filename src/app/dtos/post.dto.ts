1765fd5847f2103ae1288d5b59a8cde9da918905
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:43:56 +0600

    Refactor(post): Update DTOs for post

--- src/app/dtos/post.dto.ts
+++ src/app/dtos/post.dto.ts
@@ -1,5 +1,17 @@
 import { infer as zInfer } from 'zod';
 import { createPostSchema, updatePostSchema } from '@/app/schemas/post.schema';
+import { APIResponseDTO } from '@/app/dtos/common.dto';
+import { PostList } from '@/app/interfaces/post.interface';
+import { PostWithDetails } from '@/app/interfaces/post.interface';
+import { CursorPaginationMeta } from '@/app/interfaces/common';
 
+// Request DTOs
 export type CreatePostRequestDTO = zInfer<typeof createPostSchema>;
 export type UpdatePostRequestDTO = zInfer<typeof updatePostSchema>;
+
+// Response DTOs
+export type CreatePostResponseDTO = APIResponseDTO<{ post: PostWithDetails }>;
+export type RetrievePostResponseDTO = APIResponseDTO<{ post: PostWithDetails }>;
+export type ListPostResponseDTO = APIResponseDTO<{ posts: PostList[]; meta: CursorPaginationMeta }>;
+export type UpdatePostResponseDTO = APIResponseDTO<{ post: PostWithDetails }>;
+export type DeletePostResponseDTO = APIResponseDTO<undefined>;
