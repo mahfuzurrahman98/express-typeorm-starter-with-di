4f5145ebf0e13154aac0a9f69c03d031393fae25
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:42:48 +0600

    Refactor(post): Update routes to match new controller method names

--- src/app/routes/post.routes.ts
+++ src/app/routes/post.routes.ts
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import { container } from 'tsyringe';
 import { requireAuth } from '@/app/middlewares/auth.middleware';
 import { PostController } from '@/app/controllers/post.controller';
+import { requireSystemRole } from '@/app/middlewares/role.middleware';
+import { UserRole } from '@/app/enums/user.enum';
 
 const router: Router = Router();
 const postController = container.resolve(PostController);
 
-router.use(requireAuth);
-
-router.post('/', postController.create);
+router.post('/', requireAuth, requireSystemRole([UserRole.USER]), postController.create);
 router.get('/', postController.list);
 router.get('/:id', postController.retrieve);
-router.put('/:id', postController.update);
-router.delete('/:id', postController.delete);
+router.put('/:id', requireSystemRole([UserRole.USER]), postController.update);
+router.delete('/:id', requireSystemRole([UserRole.USER]), postController.delete);
 
 export default router;
