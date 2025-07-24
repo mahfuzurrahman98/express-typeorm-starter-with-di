1e8807f8c24205a0b60e6c89e093afd1022ea076
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:43:56 +0600

    Refactor(post): Update post schema

--- src/app/schemas/post.schema.ts
+++ src/app/schemas/post.schema.ts
@@ -1,10 +1,25 @@
-import { object, string, array } from 'zod';
+import { object, string, array, nativeEnum } from 'zod';
+import { PostSortBy, PostSortOrder } from '@/app/enums/post.enum';
 
 export const createPostSchema = object({
     title: string().trim().min(1, 'Title is required'),
     content: string().min(1, 'Content is required'),
     tags: array(string().trim().min(1, 'Tag is required')).optional(),
     categoryId: string().trim().uuid('Invalid category id'),
+});
+
+export const postListQueryParamsSchema = object({
+    q: string().trim().optional(),
+    categoryId: string().trim().uuid('Invalid category id').optional(),
+    userId: string().trim().uuid('Invalid user id').optional(),
+    tags: array(string().trim()).optional(),
+    myPosts: string().optional(),
+    sortBy: nativeEnum(PostSortOrder).optional(),
+    sortOrder: nativeEnum(PostSortBy).optional(),
+    cursor: string().optional(),
+    limit: string()
+        .transform((val) => parseInt(val, 10))
+        .optional(),
 });
 
 export const updatePostSchema = createPostSchema.partial();
