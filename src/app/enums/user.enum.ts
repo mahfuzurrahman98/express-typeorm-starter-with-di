cc312d14ae1c8a50c896eb37485628b5fdaa8b0b
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:43:56 +0600

    Feat(user): Add user enum

---
+++ src/app/enums/user.enum.ts
@@ -0,0 +1,12 @@
+export enum UserRole {
+    GUEST = 'guest',
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
+export enum UserStatus {
+    PENDING = 'pending',
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+    SUSPENDED = 'suspended',
+}
