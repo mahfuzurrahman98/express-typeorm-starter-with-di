f4861830301e250bb63c92c831f15dbd14fc4800
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:43:56 +0600

    Refactor(post): Update post interface

--- src/app/interfaces/post.interface.ts
+++ src/app/interfaces/post.interface.ts
@@ -1,17 +1,39 @@
-import { User } from '@/app/entities/user.entity';
+import { infer as zInfer } from 'zod';
+import { postListQueryParamsSchema } from '@/app/schemas/post.schema';
+
+export interface PostList {
+    id: string;
+    title: string;
+    tags: string[];
+    category: {
+        id: string;
+        name: string;
+    };
+    user: {
+        id: string;
+        email: string;
+        firstName: string;
+        lastName?: string;
+    };
+}
 
 export interface PostWithDetails {
     id: string;
     title: string;
     content: string;
     tags: string[];
-    categoryId: string;
-    userId: string;
     category: {
         id: string;
         name: string;
     };
-    user: User;
+    user: {
+        id: string;
+        email: string;
+        firstName: string;
+        lastName?: string;
+    };
     createdAt: Date;
     updatedAt: Date;
 }
+
+export interface PostListQueryParams extends zInfer<typeof postListQueryParamsSchema> {}
