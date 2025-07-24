eb419e629cb29d802d2820ee02650b5dd8436699
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:42:48 +0600

    Refactor(category): Update routes to match new controller method names

--- src/app/routes/category.routes.ts
+++ src/app/routes/category.routes.ts
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import { container } from 'tsyringe';
 import { requireAuth } from '@/app/middlewares/auth.middleware';
 import { CategoryController } from '@/app/controllers/category.controller';
+import { requireSystemRole } from '@/app/middlewares/role.middleware';
+import { UserRole } from '@/app/enums/user.enum';
 
 const router: Router = Router();
 const categoryController = container.resolve(CategoryController);
 
-router.use(requireAuth);
-
-router.post('/', categoryController.create);
+router.post('/', requireAuth, requireSystemRole(UserRole.ADMIN), categoryController.create);
 router.get('/', categoryController.list);
 router.get('/:id', categoryController.retrieve);
-router.put('/:id', categoryController.update);
-router.delete('/:id', categoryController.delete);
+router.put('/:id', requireAuth, requireSystemRole(UserRole.ADMIN), categoryController.update);
+router.delete('/:id', requireAuth, requireSystemRole(UserRole.ADMIN), categoryController.delete);
 
 export default router;
