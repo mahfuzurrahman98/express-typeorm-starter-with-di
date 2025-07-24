2adf0b09d5209582c6472b4b5446a9ec9bda088e
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:42:48 +0600

    Refactor(category): Standardize controller methods, update response types, and pass user object to services

--- src/app/controllers/category.controller.ts
+++ src/app/controllers/category.controller.ts
@@ -1,18 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { autoInjectable } from 'tsyringe';
 import { CategoryService } from '@/app/services/category.service';
-import { CreateCategoryRequestDTO, UpdateCategoryRequestDTO } from '@/app/dtos/category.dto';
-import { APIResponseDTO } from '@/app/dtos/common.dto';
-import { Category } from '@/app/entities/category.entity';
-
-type CreateCategoryResponseDTO = APIResponseDTO<{ category: Category }>;
-type RetrieveCategoryResponseDTO = APIResponseDTO<{ category: Category }>;
-type ListCategoryResponseDTO = APIResponseDTO<{ categories: Category[] }>;
-type UpdateCategoryResponseDTO = APIResponseDTO<{ category: Category }>;
-type DeleteCategoryResponseDTO = APIResponseDTO<null>;
+import {
+    CreateCategoryRequestDTO,
+    CreateCategoryResponseDTO,
+    ListCategoryResponseDTO,
+    RetrieveCategoryResponseDTO,
+    UpdateCategoryRequestDTO,
+    UpdateCategoryResponseDTO,
+} from '@/app/dtos/category.dto';
 import { createCategorySchema, updateCategorySchema } from '@/app/schemas/category.schema';
 import { formatError } from '@/utils/helpers/error-formatter';
-import { RequestUser } from '@/app/interfaces/auth.interface';
 
 @autoInjectable()
 export class CategoryController {
@@ -24,53 +22,75 @@ export class CategoryController {
         next: NextFunction,
     ) => {
         try {
-            const { id } = request.user as RequestUser;
             const data: CreateCategoryRequestDTO = createCategorySchema.parse(request.body);
-            const category = await this.categoryService.createCategory(id, data);
+            const category = await this.categoryService.createCategory(data);
             response
                 .status(201)
                 .json({ message: 'Category created successfully', data: { category } });
-        } catch (error) {
+        } catch (error: any) {
             next(formatError(error));
         }
     };
 
-    retrieve = async (request: Request, response: Response, next: NextFunction) => {
+    retrieve = async (
+        request: Request<{ id: string }>,
+        response: Response<RetrieveCategoryResponseDTO>,
+        next: NextFunction,
+    ) => {
         try {
             const { id } = request.params;
             const category = await this.categoryService.getCategoryById(id);
-            response.status(200).json({ data: category });
-        } catch (error) {
+            response
+                .status(200)
+                .json({ message: 'Category retrieved successfully', data: { category } });
+        } catch (error: any) {
             next(formatError(error));
         }
     };
 
-    list = async (request: Request, response: Response, next: NextFunction) => {
+    list = async (
+        request: Request<{}, {}, {}, { name?: string }>,
+        response: Response<ListCategoryResponseDTO>,
+        next: NextFunction,
+    ) => {
         try {
-            const categories = await this.categoryService.getAllCategories();
-            response.status(200).json({ data: categories });
-        } catch (error) {
+            const queryParams = request.query;
+            const categories = await this.categoryService.getAllCategories(queryParams);
+            response
+                .status(200)
+                .json({ message: 'Categories retrieved successfully', data: { categories } });
+        } catch (error: any) {
             next(formatError(error));
         }
     };
 
-    update = async (request: Request, response: Response, next: NextFunction) => {
+    update = async (
+        request: Request<{ id: string }, {}, UpdateCategoryRequestDTO>,
+        response: Response<UpdateCategoryResponseDTO>,
+        next: NextFunction,
+    ) => {
         try {
             const { id } = request.params;
             const data: UpdateCategoryRequestDTO = updateCategorySchema.parse(request.body);
-            const category = await this.categoryService.updateCategory(id, data);
-            response.status(200).json({ message: 'Category updated successfully', data: category });
-        } catch (error) {
+            const category = await this.categoryService.updateCategory({ id, data });
+            response
+                .status(200)
+                .json({ message: 'Category updated successfully', data: { category } });
+        } catch (error: any) {
             next(formatError(error));
         }
     };
 
-    delete = async (request: Request, response: Response, next: NextFunction) => {
+    delete = async (
+        request: Request<{ id: string }>,
+        response: Response<{ message: string }>,
+        next: NextFunction,
+    ) => {
         try {
             const { id } = request.params;
             await this.categoryService.deleteCategory(id);
             response.status(200).json({ message: 'Category deleted successfully' });
-        } catch (error) {
+        } catch (error: any) {
             next(formatError(error));
         }
     };
