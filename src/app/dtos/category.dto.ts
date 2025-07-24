c4a26942b97cb9e0b2b545d70262143bc7453515
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-07-25 03:43:56 +0600

    Refactor(category): Update DTOs for category

--- src/app/dtos/category.dto.ts
+++ src/app/dtos/category.dto.ts
@@ -1,5 +1,14 @@
 import { infer as zInfer } from 'zod';
 import { createCategorySchema, updateCategorySchema } from '@/app/schemas/category.schema';
+import { APIResponseDTO } from '@/app/dtos/common.dto';
+import { Category } from '@/app/entities/category.entity';
 
+// Request DTOs
 export type CreateCategoryRequestDTO = zInfer<typeof createCategorySchema>;
 export type UpdateCategoryRequestDTO = zInfer<typeof updateCategorySchema>;
+
+// Response DTOs
+export type CreateCategoryResponseDTO = APIResponseDTO<{ category: Category }>;
+export type RetrieveCategoryResponseDTO = APIResponseDTO<{ category: Category }>;
+export type ListCategoryResponseDTO = APIResponseDTO<{ categories: Category[] }>;
+export type UpdateCategoryResponseDTO = APIResponseDTO<{ category: Category }>;
